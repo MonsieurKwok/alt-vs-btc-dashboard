893f6bd5b6769790b3ac19788ca47ea4305dbac7
MonsieurKwok <102764541+MonsieurKwok@users.noreply.github.com>
2025-05-25 22:49:11 +0200

    Update signal_dashboard_secure_auth.py

--- signal_dashboard_secure_auth.py
+++ signal_dashboard_secure_auth.py
@@ -29,6 +29,13 @@ if authenticator.authentication_status:
     authenticator.logout("Se déconnecter", "sidebar")
 
 
+if authenticator.authentication_status:
+    name = authenticator.name
+    username = authenticator.username
+    st.sidebar.success(f"Connecté en tant que {name}")
+    authenticator.logout("Se déconnecter", "sidebar")
+
+
 if authentication_status is False:
     st.error("Nom d'utilisateur / mot de passe incorrect")
 elif authentication_status is None:
